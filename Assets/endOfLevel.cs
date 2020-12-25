151c8977d69f76c9e6e65485014ad071354dd469
phytanga <thoryvictory@gmail.com>
2020-12-25 01:20:44 -0300

    Calice giratorio

---
+++ Assets/endOfLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class endOfLevel : MonoBehaviour
+{
+    public void endOfGame()
+    {
+        //Application.Quit();
+        SceneManager.LoadScene("fps");
+        
+    }
+}
